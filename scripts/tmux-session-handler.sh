a62cb5a34fc147c522ef1dcfb59b1fce09ac3df3
David-Alexandre <beaupreda@gmail.com>
2025-11-02 08:08:31 -0500

    Use fd instead of find for tmux session handler script.

--- scripts/tmux-session-handler.sh
+++ scripts/tmux-session-handler.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
 
+DIRECTORIES=(
+	"$HOME"
+	"$HOME/codex"
+	"$HOME/config-files"
+)
+
 # One argument was provided, select it as a directory.
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find "$HOME" "$HOME/codex" -maxdepth 1 -type d | sed "s|^$HOME/||" | fzf)
+	selected=$(fd . "${DIRECTORIES[@]}" --type dir --max-depth 1 --full-path --base-directory $HOME | sed "s|^$HOME/||" | fzf)
     # Add $HOME back to the selected directory.
     [[ $selected ]] && selected="$HOME/$selected"
 fi
@@ -23,7 +29,7 @@ if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
     exit 0
 fi
 
-# If there's no session for the current directorym, create it.
+# If there's no session for the current directory, create it.
 if ! tmux has-session -t "$selected_name"; then
     tmux new-session -ds "$selected_name" -c "$selected"
     tmux select-window -t "$selected_name:1"
